7f4948cae79f049a4744032f8d338833d8d27532
Christopher Kaster <me@atomicptr.de>
2025-02-16 13:57:08 +0100

    update rapture

--- src/rapture.hpp
+++ src/rapture.hpp
@@ -8,7 +8,7 @@
     #define RAPTURE_PRINT_FUNC(...) xyz::println(std::cerr, __VA_ARGS__)
 #endif
 
-#define rpt_assert(condition, ...)                                          \
+#define rpt_assert(condition, ...)                                      \
     if (!(condition)) {                                                 \
         auto rapture_loc = std::source_location::current();             \
         RAPTURE_PRINT_FUNC("");                                         \
@@ -21,10 +21,10 @@
         RAPTURE_PRINT_FUNC("\tAssert :\t{}", (#condition));             \
         RAPTURE_PRINT_FUNC("\tMessage:\t{}", std::format(__VA_ARGS__)); \
         RAPTURE_PRINT_FUNC("");                                         \
-        std::abort();                                                   \
+        std::exit(1);                                                   \
     }
 
-#define rpt_expect(condition)                                       \
+#define rpt_expect(condition)                                   \
     if (!(condition)) {                                         \
         auto rapture_loc = std::source_location::current();     \
         RAPTURE_PRINT_FUNC("");                                 \
@@ -36,10 +36,10 @@
         );                                                      \
         RAPTURE_PRINT_FUNC("\tExpectation:\t{}", (#condition)); \
         RAPTURE_PRINT_FUNC("");                                 \
-        std::abort();                                           \
+        std::exit(1);                                           \
     }
 
-#define rpt_panic(...)                                                                                                  \
+#define rpt_panic(...)                                                                                              \
     {                                                                                                               \
         auto rapture_loc = std::source_location::current();                                                         \
         RAPTURE_PRINT_FUNC(                                                                                         \
@@ -47,10 +47,10 @@
         );                                                                                                          \
         RAPTURE_PRINT_FUNC("\tMessage:\t{}", std::format(__VA_ARGS__));                                             \
         RAPTURE_PRINT_FUNC("");                                                                                     \
-        std::abort();                                                                                               \
+        std::exit(1);                                                                                               \
     }
 
-#define rpt_unreachable()                                                                                       \
+#define rpt_unreachable()                                                                                   \
     {                                                                                                       \
         auto rapture_loc = std::source_location::current();                                                 \
         RAPTURE_PRINT_FUNC(                                                                                 \
@@ -62,10 +62,10 @@
         );                                                                                                  \
         RAPTURE_PRINT_FUNC("\tMessage:\tThis point was marked as unreachable, but was reached regardless"); \
         RAPTURE_PRINT_FUNC("");                                                                             \
-        std::abort();                                                                                       \
+        std::exit(1);                                                                                       \
     }
 
-#define rpt_unimplemented()                                                         \
+#define rpt_unimplemented()                                                     \
     {                                                                           \
         auto rapture_loc = std::source_location::current();                     \
         RAPTURE_PRINT_FUNC(                                                     \
@@ -76,5 +76,5 @@
         );                                                                      \
         RAPTURE_PRINT_FUNC("\tMessage:\tThis feature is not yet implemented."); \
         RAPTURE_PRINT_FUNC("");                                                 \
-        std::abort();                                                           \
+        std::exit(1);                                                           \
     }
